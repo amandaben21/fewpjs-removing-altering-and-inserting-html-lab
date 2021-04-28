de6f7a8dd322260d26c1353b312e0f2312b3b559
Amanda <amandaben21@aol.com>
2021-04-28 17:53:24 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+// We remove the node element here
+
+document.getElementById("main").remove() //no longer has DOM node 'main#main'
+
+// We create a new element here and assign it to a newHeader 
+
+let newHeader = document.createElement("h1") //has a 'newHeader' variable that points to node 'h1#victory'
+newHeader.id = "victory" //has a 'newHeader' variable that points to node 'h1#victory'
+newHeader.innerHTML = "Amanda is the champion" // has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
